b677f20dcfdac2bdf395ecbd3a1fe8ed0d4ff3df
ddc <ddc@users.noreply.github.com>
2019-10-26 17:14:26 -0300

    update

--- src/databases/postgres/connection.py
+++ src/databases/postgres/connection.py
@@ -17,11 +17,10 @@ class PostgreSQL:
     ################################################################################
     async def create_connection(self):
         try:
-            conn = await asyncpg.connect(user=self.bot.settings["DBUsername"],
-                                         password=self.bot.settings["DBPassword"],
-                                         database=self.bot.settings["DBName"],
-                                         port=self.bot.settings["DBPort"],
-                                         host=self.bot.settings["DBHost"])
+            conn = await self._get_connection()
+        except asyncpg.InvalidCatalogNameError as e:
+            await self.create_database(self.bot.settings["DBName"])
+            conn = await self._get_connection()
         except Exception as e:
             conn = None
             msg = f"PostgreSQL:({e.args})"
@@ -58,7 +57,6 @@ class PostgreSQL:
         conn = await self.create_connection()
         if conn is not None:
             try:
-
                 result_set = await conn.fetch(sql)
                 for lineNumber, data in enumerate(result_set):
                     final_data[lineNumber] = dict(data)
@@ -68,3 +66,33 @@ class PostgreSQL:
             finally:
                 await conn.close()
                 return final_data
+
+    ################################################################################
+    async def create_database(self, db_name: str):
+        conn = await self._get_connection(db_name=False)
+        sql = f"CREATE DATABASE \"{db_name}\""
+        if conn is not None:
+            try:
+                await conn.execute(sql)
+                self.bot.log.info(f"Database: {db_name} created.")
+            except Exception as e:
+                self.bot.log.exception("PostgreSQL", exc_info=e)
+                self.bot.log.error(f"Sql:({sql})")
+                print(str(e))
+            finally:
+                await conn.close()
+
+    ################################################################################
+    async def _get_connection(self, db_name=True):
+        if db_name:
+            conn = await asyncpg.connect(user=self.bot.settings["DBUsername"],
+                                         password=self.bot.settings["DBPassword"],
+                                         database=self.bot.settings["DBName"],
+                                         port=self.bot.settings["DBPort"],
+                                         host=self.bot.settings["DBHost"])
+        else:
+            conn = await asyncpg.connect(user=self.bot.settings["DBUsername"],
+                                         password=self.bot.settings["DBPassword"],
+                                         port=self.bot.settings["DBPort"],
+                                         host=self.bot.settings["DBHost"])
+        return conn
