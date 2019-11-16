f804bbe5e21277a44fb146d4225949b888968c24
ddc <ddc@users.noreply.github.com>
2019-11-16 17:52:38 -0300

    update

--- requirements/install_reqs_root.sh
+++ requirements/install_reqs_root.sh
@@ -5,6 +5,7 @@ PYTHON3="/usr/bin/python3"
 PKG_CONFIG_PATH=/opt/vc/lib/pkgconfig:/usr/lib/arm-linux-gnueabihf/pkgconfig:/usr/lib/pkgconfig:/usr/share/pkgconfig:/usr/local/lib/pkgconfig:/usr/local/pgsql/lib/pkgconfig:$PKG_CONFIG_PATH:
 export PKG_CONFIG_PATH
 
+sudo apt-get update
 sudo apt-get install -y libsqlite3-dev
 sudo apt-get install -y libperl-dev
 sudo apt-get install -y python3-pip
