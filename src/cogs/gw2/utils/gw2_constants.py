736d90995fc81b1c978d3a13b35b21318b019ffa
ddc <ddc@users.noreply.github.com>
2019-11-17 22:31:58 -0300

    update

--- src/cogs/gw2/utils/gw2_constants.py
+++ src/cogs/gw2/utils/gw2_constants.py
@@ -7,7 +7,13 @@
 # |*****************************************************
 # # -*- coding: utf-8 -*-
 
-GW2_SETTINGS_FILENAME = "config/gw2_settings.ini"
+from src.cogs.bot.utils import constants
+
+if constants.IS_WINDOWS:
+    GW2_SETTINGS_FILENAME = f"{constants.root_path}\\config\\gw2_settings.ini"
+else:
+    GW2_SETTINGS_FILENAME = f"{constants.root_path}/config/gw2_settings.ini"
+
 WIKI_URL = "https://wiki.guildwars2.com"
 GW2_WIKI_ICON_URL = "https://wiki.guildwars.com/images/b/bf/Normal_gw2logo.jpg"
 GW2_SETTINGS_URL_FILE = "https://raw.githubusercontent.com/ddc/DiscordBot/master/config/gw2_settings.ini"
