7981b73926077b276abee4a30cb1ab2072a5804b
ddc <ddc@users.noreply.github.com>
2024-04-22 16:24:41 -0300

    feat: bump_version 2.0.5

--- src/bot/constants/variables.py
+++ src/bot/constants/variables.py
@@ -4,26 +4,11 @@ import os
 import sys
 from pathlib import Path
 from src.bot.constants.configs import Configs
+from ddcUtils import ConfFileUtils
+
 
 _env_configs = Configs()
 DEBUG = _env_configs.debug
-DEFAULT_PREFIX = _env_configs.default_prefix
-ALLOWED_PREFIXES = ("!", "?", "$", "%", "&", ".")
-TIME_BEFORE_START = 5 if DEBUG is False else 0
-INTERACTIVE_MODE = len(sys.argv) <= 1
-PYTHON_OK = sys.version_info >= (3, 6)
-IS_WINDOWS = os.name == "nt"
-###############################################################################
-BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
-SETTINGS_FILENAME = os.path.join(BASE_DIR, "config", "bot_settings.ini")
-ALEMBIC_CONFIG_FILE_PATH = os.path.join(BASE_DIR, "config", "alembic.ini")
-DATABASE_FILENAME = os.path.join(BASE_DIR, "data", "database.db")
-LOGS_DIR = os.path.join(BASE_DIR, "logs")
-SQL_DIRPATH = os.path.join(BASE_DIR, "data", "sql")
-###############################################################################
-_versionFile = open(os.path.join(BASE_DIR, "VERSION"), encoding="utf-8", mode="r")
-VERSION = _versionFile.read().split("\n", 1)[0].strip("\n")
-_versionFile.close()
 ###############################################################################
 DESCRIPTION = "A Bot for Discord"
 AUTHOR_ID = "195615080665055232"
@@ -37,6 +22,25 @@ DATE_FORMATTER = "%Y-%m-%d"
 TIME_FORMATTER = "%H:%M:%S.%f"
 ###############################################################################
 GAMES_INCLUDED = ("Guild Wars 2",)
+###############################################################################
+DEFAULT_PREFIX = _env_configs.default_prefix
+ALLOWED_PREFIXES = ("!", "?", "$", "%", "&", ".")
+TIME_BEFORE_START = 5 if DEBUG is False else 0
+INTERACTIVE_MODE = len(sys.argv) <= 1
+IS_WINDOWS = os.name == "nt"
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+###############################################################################
+_python_req_version = ConfFileUtils().get_value(os.path.join(BASE_DIR, "pyproject.toml"), "tool.poetry.dependencies", "python")
+_major = int(_python_req_version.split(".")[0].replace("^", ""))
+_minor = int(_python_req_version.split(".")[1])
+PYTHON_OK = sys.version_info >= (_major, _minor)
+###############################################################################
+VERSION = ConfFileUtils().get_value(os.path.join(BASE_DIR, "pyproject.toml"), "tool.poetry", "version")
+SETTINGS_FILENAME = os.path.join(BASE_DIR, "config", "bot_settings.ini")
+ALEMBIC_CONFIG_FILE_PATH = os.path.join(BASE_DIR, "config", "alembic.ini")
+DATABASE_FILENAME = os.path.join(BASE_DIR, "data", "database.db")
+LOGS_DIR = os.path.join(BASE_DIR, "logs")
+SQL_DIRPATH = os.path.join(BASE_DIR, "data", "sql")
 ###############################################################################
 _bot_cogs = [os.path.join("src", "bot", "admin", "admin.py")] # loading admin group cog first
 _bot_cogs += [x for x in glob.glob(os.path.join("src", "bot", "*.py"))]
