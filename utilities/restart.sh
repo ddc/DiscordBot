2e530fec7c9a5e3dc005127b16a070a7b3dca797
ddc <ddc@users.noreply.github.com>
2025-04-22 14:04:08 -0300

    v2.0.19

--- utilities/restart.sh
+++ utilities/restart.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+PROJECT_DIR=/opt/DiscordBot
+pushd "$PROJECT_DIR" || exit
+
+docker compose down
 docker rmi $(docker images 'discordbot*' -a -q) -f
 docker compose up -d --build --force-recreate
+
+popd || exit
