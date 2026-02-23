7312e96defc3f9d688b674e8da54f8b704780020
ddc <34492089+ddc@users.noreply.github.com>
2026-02-23 19:38:18 -0300

    v3.0.4

--- utilities/update.sh
+++ utilities/update.sh
@@ -18,6 +18,6 @@ sudo chmod 600 "$PROJECT_DIR/.env"
 sudo chmod 755 "$PROJECT_DIR/utilities"/*.sh
 
 # ensure logs dir is writable by container's botuser (uid 1000)
-sudo chown 1000:1000 "$PROJECT_DIR/logs"
+sudo chown -R 1000:1000 "$PROJECT_DIR/logs"
 
 popd > /dev/null
