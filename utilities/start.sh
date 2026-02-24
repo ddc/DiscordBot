008ecfa392c6a9f61b62893155b10de1bb055cce
ddc <34492089+ddc@users.noreply.github.com>
2026-02-24 15:24:29 -0300

    v3.0.4

--- utilities/start.sh
+++ utilities/start.sh
@@ -8,7 +8,7 @@ docker compose down
 docker images 'discordbot*' -a -q | xargs -r docker rmi -f
 
 # ensure logs dir is writable by container's botuser (uid 1000)
-sudo chown 1000:1000 "$PROJECT_DIR/logs"
+sudo chown -R 1000:1000 "$PROJECT_DIR/logs"
 sudo chmod 755 "$PROJECT_DIR/logs"
 
 docker compose up -d --build --force-recreate
